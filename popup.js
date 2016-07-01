f8bd105ef08daa06b1870d422bbf5f1430e6cba5
Li Ge <lige@yahoo-inc.com>
2016-07-01 16:53:54 -0700

    Search tab works

--- popup.js
+++ popup.js
@@ -1,41 +1,125 @@
-function renderCandidate(candidate) {
+var candidateTemplate;
+var selectedIndex = 0;
+var maxNumCandidates = 20;
+var candidates = [];
 
+function renderCandidate(candidate) {
+  return Mustache.render(candidateTemplate, candidate);
 }
 
 function createTabCandidate(tab) {
   return {
     title: tab.title,
     url: tab.url,
-    thumb: tab.favIconUrl
+    thumb: tab.favIconUrl,
+    tab: tab
   };
 }
 
+function escapeRegex(str) {
+  return str.replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\-]', 'g'), '\\$&');
+}
 
-function getAllTabs() {
+function filterCandidate(query, candidate) {
+  if (query === '') return true;
+  query = escapeRegex(query);
+  let re = new RegExp(query, "i");
+  return candidate.title.search(re) >= 0 ||
+    candidate.url.search(re) >= 0;
+}
+
+function updateCandidates(callback) {
+  let query = document.querySelector('#search-box').value;
+  candidates = [];
+
   chrome.tabs.query({}, function(aTabs) {
-    let tabs = document.querySelector('#candidates');
-    aTabs.forEach(function(tab) {
-      // console.log(tab.title, tab.url);
-      let entry = document.createElement('div');
-
-      let icon = document.createElement('img');
-      icon.src = tab.favIconUrl;
-
-      let title = document.createElement('div');
-      title.textContent = tab.title;
-
-      let link = document.createElement('div');
-      link.textContent = tab.url;
-
-      // entry.appendChild(icon);
-      entry.appendChild(title);
-      entry.appendChild(link);
-      console.log(tab);
-      tabs.appendChild(entry);
-    });
+    for (let i = 0, len = aTabs.length; i < len; ++i) {
+      let tab = aTabs[i];
+      let candidate = createTabCandidate(tab);
+      if (filterCandidate(query, candidate)) {
+        candidates.push(candidate);
+      }
+      if (candidates.length >= maxNumCandidates) break;
+    }
+    callback();
   });
 }
 
+function renderCandidates() {
+  let container = document.querySelector('#candidates-body');
+  let query = document.querySelector('#search-box').value;
+
+  container.innerHTML = '';
+  candidates.forEach(function(candidate) {
+    container.innerHTML += renderCandidate(candidate);
+  });
+}
+
+function updateSelection() {
+  const elements = document.querySelectorAll('#candidates-body > tr');
+  for (let i = 0, len = elements.length; i < len; ++i) {
+    if (i === selectedIndex) {
+      elements[i].classList.add('selected');
+    } else {
+      elements[i].classList.remove('selected');
+    }
+  }
+}
+
+function update() {
+  updateCandidates(function() {
+    renderCandidates();
+    updateSelection();
+  });
+}
+
+function prevCandidate() {
+  if (selectedIndex > 0) {
+    --selectedIndex;
+    updateSelection();
+  }
+}
+
+function nextCandidate() {
+  if (selectedIndex < candidates.length - 1) {
+    ++selectedIndex;
+    updateSelection();
+  }
+}
+
+function gotoSelectedTab() {
+  const selected = candidates[selectedIndex];
+  if (selected) {
+    chrome.tabs.update(selected.tab.id, {
+      active: true,
+      selected: true
+    });
+    chrome.windows.update(selected.tab.windowId, {
+      focused: true
+    });
+    // restore state
+  }
+}
+
+function runAction(e) {
+  console.log('key', e);
+  if (e.keyCode === 13) {
+    // Enter key, goto selected tap
+    gotoSelectedTab();
+  } else if (e.keyCode === 38 || (e.ctrlKey && e.keyCode === 80)) {
+    // Up key or Ctrl-p
+    prevCandidate();
+  } else if (e.keyCode === 40 || (e.ctrlKey && e.keyCode === 78)) {
+    // Down key or Ctrl-n
+    nextCandidate();
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  // getAllTabs();
+  candidateTemplate = document.querySelector('#candidate-template').innerHTML;
+  Mustache.parse(candidateTemplate);
+
+  update();
+  document.querySelector('#search-box').addEventListener('input', update);
+  document.querySelector('#search-box').addEventListener('keydown', runAction);
 });

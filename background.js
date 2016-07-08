b2f67a7a41a2077b9c65a3523796f0a5e114f3d6
Li Ge <lige@yahoo-inc.com>
2016-07-08 10:59:02 -0700

    Open helm window or show helm window

--- background.js
+++ background.js
@@ -1,26 +1,27 @@
-let windowId = null;
-
-// TODO:
-//   - set windowId to null when closed.
-//   - focus on input field when window is focused.
-function showWindow() {
-  if (windowId !== null) {
-    chrome.windows.update(windowId, { focused: true });
-    return;
-  }
-
-  const options = {
-    type: 'popup',
-    left: 100,
-    top: 100,
-    width: 800,
-    height: 475
-  };
-
-  options.url = 'helm.html';
-  chrome.windows.create(options, (win) => {
-    windowId = win.id;
+function createOrShowHelmWindow() {
+  chrome.tabs.query({
+    title: 'Chrome Helm'
+  }, (tabs) => {
+    console.log('tabs', tabs);
+    if (tabs.length > 0) {
+      chrome.tabs.update(tabs[0].id, {
+        active: true,
+        selected: true
+      });
+      chrome.windows.update(tabs[0].windowId, {
+        focused: true
+      });
+    } else {
+      chrome.windows.create({
+        type: 'popup',
+        left: 100,
+        top: 100,
+        width: 800,
+        height: 475,
+        url: 'helm.html'
+      });
+    }
   });
 }
 
-chrome.browserAction.onClicked.addListener(showWindow);
+chrome.browserAction.onClicked.addListener(createOrShowHelmWindow);
